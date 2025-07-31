ee21ca46e0e71253ed5050b9343ef914b6e18eaf
JOTAPEnvz <jpmoraistomaz01@gmail.com>
2025-07-31 16:32:45 -0300

    feat:add home page and components

--- Esporte/src/app/_layout.tsx
+++ Esporte/src/app/_layout.tsx
@@ -1,5 +1,12 @@
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
+import "../styles/global.css";
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="auth/home" />
+      <Stack.Screen name="auth" />
+      <Stack.Screen name="public" />
+    </Stack>
+  );
 }
